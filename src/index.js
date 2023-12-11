9aa0552faa87ba1834d65b5d7aa5f8491699fb47
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-12-11 22:30:10 +0200

    g

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename="test-task">
+    <BrowserRouter basename="/test-task">
     <App />
       </BrowserRouter>
       </Provider>
